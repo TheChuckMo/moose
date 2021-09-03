af06dac1de13ebfc0ece0dea3849ab127283c85e
Charles Moreland <morelanc@ohsu.edu>
2021-09-03 14:32:08 -0700

    version 0.4.0

--- build_version.sh
+++ build_version.sh
@@ -4,37 +4,37 @@ build=${1:-patch}
 
 case $build in
   patch|minor|major|prepatch|preminor|premajor|prerelease)
-    echo build ${build}
+    echo == build ${build}
     ;;
   *)
-    echo build ${build} INVALID
+    echo == build ${build} INVALID
     exit 1
     ;;
 esac
 
-echo version patch
+echo == version patch
 poetry version ${build}
 version=$(poetry version --short)
 
-echo new ${build} version: ${version}
+echo == new ${build} version: ${version}
 
-echo update and lock
+echo == update and lock
 poetry update --lock
 
-echo export requirements.txt
+echo == export requirements.txt
 poetry export > requirements.txt
 
-echo local build
+echo == local build
 poetry build
 
-echo commit version
+echo == commit version
 git commit --all --message "version ${version}"
 
-echo version tag
+echo == version tag
 git tag "${version}"
 
-echo push commmit
+echo == push commmit
 git push --all
 
-echo push tags
+echo == push tags
 git push --tags
